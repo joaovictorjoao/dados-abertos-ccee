6274948d9f0b6ed1f14f3b804f0139ae010486ec
joaovictorjoao <joao.fiabani@gmail.com>
2026-06-12 10:06:05 -0300

    chore: consolidar API publica de grugeen_dashboards.comum

--- Mapas de Consumo/grugeen_dashboards/comum/__init__.py
+++ Mapas de Consumo/grugeen_dashboards/comum/__init__.py
@@ -1,6 +1,5 @@
 """Funções auxiliares compartilhadas entre as seções do dashboard."""
 
-from .energia import prevent_sleep, restore_sleep
 from .formato import (
     formatar_br, fmt_gwh, fmt_mwh, fmt_mwh_hab, fmt_cons_100k, fmt_pop,
 )
@@ -8,3 +7,13 @@ from .geo import normalizar, geocodificar
 from .http import fetch, baixar_recurso
 from .logging_setup import setup_logging
 from .marca import logo_data_uri
+from .energia import prevent_sleep, restore_sleep
+
+__all__ = [
+    "formatar_br", "fmt_gwh", "fmt_mwh", "fmt_mwh_hab", "fmt_cons_100k", "fmt_pop",
+    "normalizar", "geocodificar",
+    "fetch", "baixar_recurso",
+    "setup_logging",
+    "logo_data_uri",
+    "prevent_sleep", "restore_sleep",
+]
